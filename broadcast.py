92c4f099843ac3c4802cef73eea89b1249a707a5
mnushin102 <meraj_nushin@yahoo.com>
2024-03-06 20:37:34 -0800

    Network activities for the broadcast protocol have been set up to start the server

---
+++ broadcast.py
@@ -0,0 +1,34 @@
+'''
+    Meraj Nushin
+    CECS 327: Intro to Networks and Distributed Computing
+    Project 1: A Bite of Distributed Communication 
+    In this project, you will embark on the exciting journey of building 
+    a distributed system using Java/Python, Docker, and the Linux terminal. 
+    The project is divided into three main steps, each focusing on different 
+    aspects of system architecture, communication protocols, and network monitoring.
+'''
+
+# import all of these dependencies 
+import socket 
+import struct 
+import sys
+
+# Assign these network activities for the broadcast protocol design
+source_ipAddress = '192.168.1.202'
+destination_ipAddress = '172.224.42.53'
+source_port = '42992'
+destination_port = '60002'
+length = 1314
+flags = 0x010 
+
+
+
+
+
+
+
+
+
+
+
+
