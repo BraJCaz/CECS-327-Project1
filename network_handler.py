275d2418b8bc6f82c5ba2c96c525a656f2c579d0
Brandon J. Cazares <130398253+BraJCaz@users.noreply.github.com>
2024-03-08 13:05:50 -0800

    Update network_handler.py

--- network_handler.py
+++ network_handler.py
@@ -6,32 +6,36 @@ import socket
 
 class NetworkHandler: 
     # this is our init with our port address 
-    def __init__(s, host='', port=8000):
-        s.host = host 
-        s.port = port 
-        s.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def __init__(self, host='', port=8000):
+        self.host = host 
+        self.port = port 
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # this is our bind and listen from our socket to both our host and port 
-    def bind_and_listen(s):
-        s.socket.bind((s.host, s.port))
-        s.socket.listen(8)
-        print(f"Network is listening {s.host}:{s.port}")
+    def bind_and_listen(self):
+        self.socket.bind((self.host, self.port))
+        self.socket.listen(8)
+        print(f"Network is listening {self.host}:{self.port}")
     # this makes sure our connection's accepted 
-    def connection_accepted(s):
-        client_socket, address = s.socket.accept()
+    def connection_accepted(self):
+        client_socket, address = self.socket.accept()
         return client_socket, address 
     # this makes sure our message is fully sent 
-    def send(s, client_socket, m):
-        client_socket.send(m.encode())
+    def send(self, client_socket, message):
+        client_socket.send(message.encode())
     # this checks if our message is received 
-    def receive(s, client_socket, buffer_size=1024):
+    def receive(self, client_socket, buffer_size=1024):
         return client_socket.recv(buffer_size).decode()
-    # anycast is usede for both our protocols: broadcast and unicast 
-    def anycast(s, m, optimal_node):
-        s.send(optimal_node, m)
+    # this is our broadcast handler because that's our first protocol we chose 
+    def broadcast(self, message, all_nodes):
+        for node in all_nodes:
+            self.send(node, message) 
+    # anycast is usede for both our protocols: broadcast and unicast
+    def anycast(self, message, optimal_node):
+        self.send(optimal_node, message)
     # we're not doing multicast for this project 
-    def multicast(s, m, group_nodes):
+    def multicast(self, message, group_nodes):
         for node in group_nodes:
-            s.send(node, m)
+            self.send(node, message)
     # this will close our network handler code 
-    def close(s, client_socket):
+    def close(self, client_socket):
         client_socket.close() 
