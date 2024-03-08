d7670b015666f2da8936728980d5a0f9289e433f
Brandon Jeremy Cazares <brandoncazares@dhcp-39-80-6.dhcpint.csulb.edu>
2024-03-08 11:58:54 -0800

    <comments

--- main_server.py
+++ main_server.py
@@ -8,15 +8,9 @@ logging.basicConfig(filename='project.log', level=logging.DEBUG,
                     format='%a(asctime)s - %(levelname)s - %(message)s')
 
 # this will start our server 
-<<<<<<< HEAD
 def start_server(port):
     "This will initialixed and run the distributed server on the port we're going to use."
     s = distributedserver(port=port)
-=======
-def start_server(port):
-    "This will initialixed and run the distributed server on the port we're going to use."
-    s = distributedserver(port=port)
->>>>>>> 8ebf020 (<comments)
     s.run() 
 
 # this is our main code 
@@ -24,11 +18,7 @@ def main():
     # This is our passed argument from our terminal for customization 
     parser = argparse.ArgumentParser(
         description="The main server's starting for our distributed networking project.")
-<<<<<<< HEAD
-    parser.add_argument("--port," type=int, default=8000,
-=======
     parser.add_argument("--port", type=int, default=8000,
->>>>>>> 8ebf020 (<comments)
                         help="Port for the server to run on it.")
     
     args = parser.parse_args()
