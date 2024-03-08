4661bdf6517d151cfc1b05de5d7028866f18d86d
Brandon J. Cazares <130398253+BraJCaz@users.noreply.github.com>
2024-03-08 12:35:10 -0800

    Update distrubted_server.py

--- distrubted_server.py
+++ distrubted_server.py
@@ -7,43 +7,43 @@ import socket
 class DistributedServer:
 
     # now, we need to define both our host and port 
-    def __intit__(s, host='', port=8000):
-        s.host = host
-        s.port = port 
-        s.server_socket = None 
+    def __intit__(self, host='', port=8000):
+        self.host = host
+        self.port = port 
+        self.server_socket = None 
 
     # we need to start our self code 
-    def start(s):
-        s.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.server_socket = socket.bind((s.host, s.port))
-        s.server_socket.listen(5)
-        print(f"Server is starting on {s.host}:{s.port}")
+    def start(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket = socket.bind((self.host, self.port))
+        self.server_socket.listen(8)
+        print(f"Server is starting on {self.host}:{self.port}")
 
     # we need to handle our client from our starting server 
-    def handle_client(s, client_socket):
+    def handle_client(self, client_socket):
         # This will define our logic to make sure our incoming client connections and messages 
-        m = client_socket.recveiver(1024).decode()
-        print(f"Message received: {m}")
+        message = client_socket.recveiver(1024).decode()
+        print(f"Message received: {message}")
 
         # this sends back a confirmation message 
-        confirm_m = "Message was successfully received!"
-        client_socket.send(confirm_m.encode())
+        confirm_message = "Message was successfully received!"
+        client_socket.send(confirm_message.encode())
 
         # then, we need to further process both our protocols which can be added here
         client_socket.close()
 
         # we need to do another test run to find out if our client's connected to our distributed server 
-        def run(s):
-            s.start()
+        def run(self):
+            self.start()
             while True:
-                client_socket, address = s.server_socket.accept()
+                client_socket, address = self.server_socket.accept()
                 print(f"Connection from {address}")
-                s.handle_client(client_socket)
+                self.handle_client(client_socket)
 
     ## this is our main code to test our distributed server code 
     if __name__ == "__main__":
-        s = DistributedServer(port=8001)
-        s.run()
+        server = DistributedServer(port=8001)
+        server.run()
 
 
         
