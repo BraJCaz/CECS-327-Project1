7e0e800b2e60fce50151ba6893e9d6ab6bbb3396
Brandon J. Cazares <130398253+BraJCaz@users.noreply.github.com>
2024-03-07 21:25:20 -0800

    Add files via upload

---
+++ main_server.py
@@ -0,0 +1,30 @@
+# this is our main server file 
+import argparse
+from distrubted_server import distributedserver
+import logging 
+
+# This will configure logging 
+logging.basicConfig(filename='project.log', level=logging.DEBUG, 
+                    format='%a(asctime)s - %(levelname)s - %(message)s')
+
+# this will start our server 
+def start_server(p):
+    "This will initialixed and run the distributed server on the port we're going to use."
+    s = distributedserver(p=p)
+    s.run() 
+
+# this is our main code 
+def main(): 
+    # This is our passed argument from our terminal for customization 
+    parser = argparse.ArgumentParser(
+        description="The main server's starting for our distributed networking project.")
+    parser.add_argument("--port," type=int, default=8000,
+                        help="Port for the server to run on it.")
+    
+    args = parser.parse_args()
+
+    # This is starting the server with specified (or default) arguments 
+    start_server(args.port)
+
+if __name__ == "__main__":
+    main() 
